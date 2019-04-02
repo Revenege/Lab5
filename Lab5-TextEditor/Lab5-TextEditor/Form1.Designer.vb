9aa378c8f6dd70c5b03158ac21a8063bbb2b022c
Revenege <47994768+Revenege@users.noreply.github.com>
2019-04-02 18:30:03 -0400

    Initial Commit
    Contains all code for Lab5

---
+++ Lab5-TextEditor/Lab5-TextEditor/Form1.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FrmTextEditor
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.menuTextEditor = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveAsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CopyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PasteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.saveDialog = New System.Windows.Forms.SaveFileDialog()
+        Me.openDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.menuTextEditor.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'menuTextEditor
+        '
+        Me.menuTextEditor.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible
+        Me.menuTextEditor.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.menuTextEditor.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.menuTextEditor.Location = New System.Drawing.Point(0, 0)
+        Me.menuTextEditor.Name = "menuTextEditor"
+        Me.menuTextEditor.Padding = New System.Windows.Forms.Padding(4, 2, 0, 2)
+        Me.menuTextEditor.Size = New System.Drawing.Size(1067, 28)
+        Me.menuTextEditor.TabIndex = 0
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.OpenToolStripMenuItem, Me.SaveToolStripMenuItem, Me.SaveAsToolStripMenuItem, Me.ToolStripMenuItem1, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(44, 24)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(135, 26)
+        Me.NewToolStripMenuItem.Text = "&New"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(135, 26)
+        Me.OpenToolStripMenuItem.Text = "&Open"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(135, 26)
+        Me.SaveToolStripMenuItem.Text = "&Save"
+        '
+        'SaveAsToolStripMenuItem
+        '
+        Me.SaveAsToolStripMenuItem.Name = "SaveAsToolStripMenuItem"
+        Me.SaveAsToolStripMenuItem.Size = New System.Drawing.Size(135, 26)
+        Me.SaveAsToolStripMenuItem.Text = "Save As"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(132, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(135, 26)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CopyToolStripMenuItem, Me.CutToolStripMenuItem, Me.PasteToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(47, 24)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'CopyToolStripMenuItem
+        '
+        Me.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        Me.CopyToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
+        Me.CopyToolStripMenuItem.Text = "&Copy"
+        '
+        'CutToolStripMenuItem
+        '
+        Me.CutToolStripMenuItem.Name = "CutToolStripMenuItem"
+        Me.CutToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
+        Me.CutToolStripMenuItem.Text = "Cu&t"
+        '
+        'PasteToolStripMenuItem
+        '
+        Me.PasteToolStripMenuItem.Name = "PasteToolStripMenuItem"
+        Me.PasteToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
+        Me.PasteToolStripMenuItem.Text = "&Paste"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(53, 24)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(125, 26)
+        Me.AboutToolStripMenuItem.Text = "&About"
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(0, 33)
+        Me.txtInput.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtInput.Multiline = True
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtInput.Size = New System.Drawing.Size(1065, 516)
+        Me.txtInput.TabIndex = 1
+        '
+        'openDialog
+        '
+        Me.openDialog.FileName = "OpenFileDialog1"
+        '
+        'FrmTextEditor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1067, 554)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.menuTextEditor)
+        Me.MainMenuStrip = Me.menuTextEditor
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "FrmTextEditor"
+        Me.Text = "Amazing Text Editor - New File"
+        Me.menuTextEditor.ResumeLayout(False)
+        Me.menuTextEditor.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents menuTextEditor As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveAsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PasteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents saveDialog As SaveFileDialog
+    Friend WithEvents openDialog As OpenFileDialog
+End Class
---
+++ Lab5-TextEditor/Lab5-TextEditor/Form1.vb
@@ -0,0 +1,136 @@
+﻿Imports System.IO
+Public Class FrmTextEditor
+
+    Dim filename As String = ""
+    Dim txtChanged As Boolean = False
+    Dim formIsClosing As Boolean = False
+
+    'Handles Sav As Function. Public Function so it can be called by both the Save As event handler
+    'and the Save event handler(in case there is no file)
+    Public Sub SaveAs()
+        saveDialog.Filter = "Plain Text Files(*.txt) | *.txt" 'Sets the type of files being searched for
+        If saveDialog.ShowDialog() = DialogResult.OK Then 'If the dialog has a valid file path, then save the file
+            filename = saveDialog.FileName
+            My.Computer.FileSystem.WriteAllText(filename, txtInput.Text, False) 'saves to the file, deleting previous contents
+            txtChanged = False
+            Me.Text = "Awesome Text Editor - " + filename
+        End If
+    End Sub
+
+
+    Public Function CloseForm() As Boolean
+        If txtChanged = True Then
+            Dim selection = MsgBox("Are you sure you want to end the program? All unsaved progress will be lost", MsgBoxStyle.YesNo Or MsgBoxStyle.Exclamation, "Warning, Unsaved Changes!")
+            If selection = vbYes Then
+                Return True
+            Else
+                Return False
+            End If
+        Else
+            Return True
+        End If
+    End Function
+
+
+    'Event Handler for when the user clicks File -> New
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        If txtChanged = True Then
+            Dim selection = MsgBox("Are you sure you want to create a new document? All unsaved progress will be lost", MsgBoxStyle.YesNo Or MsgBoxStyle.Exclamation, "Warning, Unsaved Changes!")
+            If selection = vbYes Then
+                txtInput.Text = ""
+                filename = ""
+                Me.Text = "Awesome Text Editor - New File"
+            End If
+        Else
+            txtInput.Text = ""
+            filename = ""
+            Me.Text = "Awesome Text Editor - New File"
+        End If
+    End Sub
+
+    'Event Handler for when the user clicks File -> Open
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        If txtChanged = True Then
+            Dim selection = MsgBox("Are you sure you want to open a new document? All unsaved progress will be lost", MsgBoxStyle.YesNo Or MsgBoxStyle.Exclamation, "Warning, Unsaved Changes!")
+            If selection = vbYes Then
+                openDialog.Filter = "Plain Text Files (*.txt) | *.txt" 'Sets the type of files being searched for
+                If openDialog.ShowDialog() = DialogResult.OK Then 'if the dialog has valid results, then open the file and set the filename
+                    filename = openDialog.FileName
+                    txtInput.Text = ""
+                    Using reader As StreamReader = New StreamReader(filename)
+                        Do While (reader.Peek() > -1)
+                            txtInput.AppendText(reader.ReadLine())
+                            txtInput.AppendText(vbNewLine)
+                        Loop
+                    End Using
+
+                    Me.Text = "Awesome Text Editor - New File"
+                End If
+            End If
+        Else
+            openDialog.Filter = "Plain Text Files (*.txt) | *.txt" 'Sets the type of files being searched for
+            If openDialog.ShowDialog() = DialogResult.OK Then 'if the dialog has valid results, then open the file and set the filename
+                filename = openDialog.FileName
+                txtInput.Text = My.Computer.FileSystem.ReadAllText(filename) 'reading the file
+
+                Me.Text = "Awesome Text Editor - New File"
+            End If
+        End If
+    End Sub
+
+    'Event Handler for when the user clicks File -> Exit
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        If (CloseForm()) Then
+            formIsClosing = True
+            Me.Close()
+        End If
+    End Sub
+
+    'Event Handler for when the user clicks File -> Save
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        If filename = "" Then
+            SaveAs()
+        Else
+            My.Computer.FileSystem.WriteAllText(filename, txtInput.Text, False)
+        End If
+    End Sub
+    'Event Handler for when the user clicks File -> Save As
+    Private Sub SaveAsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveAsToolStripMenuItem.Click
+        SaveAs()
+    End Sub
+
+    'If Text has been changed, set our text changed variable to true.
+    Private Sub TxtInput_TextChanged(sender As Object, e As EventArgs) Handles txtInput.TextChanged
+        txtChanged = True
+    End Sub
+
+    Private Sub HelpToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HelpToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub FormClose(sender As Object, e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        If (formIsClosing = False) Then
+            If txtChanged = True Then
+                Dim selection = MsgBox("Are you sure you want to end the program? All unsaved progress will be lost", MsgBoxStyle.YesNo Or MsgBoxStyle.Exclamation, "Warning, Unsaved Changes!")
+                If selection = vbNo Then
+                    e.Cancel = True
+                End If
+            End If
+        End If
+    End Sub
+
+    'Copy Contents of Select Location to Clipbaord
+    Private Sub CopyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyToolStripMenuItem.Click
+        txtInput.Copy()
+    End Sub
+
+    'Cut contents of selected location to Clipboard
+    Private Sub CutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CutToolStripMenuItem.Click
+        txtInput.Cut()
+    End Sub
+
+    'Paste Contents of Clipboard to selected location
+    Private Sub PasteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PasteToolStripMenuItem.Click
+        txtInput.Paste()
+    End Sub
+End Class
